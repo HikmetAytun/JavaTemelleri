c947db027c0ec29fd41b52c54ded353fee449c0d
Hikmet AYTUN <hikmet_aytun@ibb.net>
2026-09-09 17:11:52 +0300

    Functions

--- html-css-note/faktoriyel.js
+++ html-css-note/faktoriyel.js
@@ -1,10 +1,10 @@
 //faktöriyel bulma
 //Değişkenleri tanımlayalım
-let sayac, f=1, n;
+let sayac, f=1, n=3;
 //Kullanıcıdan hangi sayının faktöriyeli alacağını isteyelim.
-n=Number(prompt("n: "));
+// n=Number(prompt("n: "));
 //For döngüsü
 for(sayac=1;sayac<=n;sayac++)
     f=f*sayac;
 
-console.log(n+"!= "+f);
+console.log(n+"! = "+f);
