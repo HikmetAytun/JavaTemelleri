02d953dfab4c0b14803eeb679cd8d1982faf01e0
Hikmet AYTUN <hikmet_aytun@ibb.net>
2026-09-10 17:05:02 +0300

    10.09.2026 calismalari eklendi.

---
+++ html-css-note/HariciDosya.js
@@ -0,0 +1,19 @@
+function kare(x) {
+    return x * x;
+}
+
+function kup(x) {
+    return x * x * x;
+}
+
+function topla(a,b) {
+    return a + b;
+}
+
+function carp(a,b) {
+    return a * b;
+}   
+
+function yaz(deger){        //fonksiyon tanımlanır ve parametre olarak deger alınır.
+    document.write(deger+"<br>");
+}
